d12bdc2bbf5c64129ec55b5fe769c2673682312f
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-09-19 07:23:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //Star Pattern Printing Code
+            int x;
+            int y;
+            int num;
+            Console.WriteLine("Enter Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (x = 0; x < num; x++)
+            {
+                for (y = 0; y < x; y++)
+
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
+
+
