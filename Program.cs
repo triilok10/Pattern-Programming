adfb7753bc05c38ebe4bb6ec4f479d56f3946a95
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-09-19 07:50:19 +0530

    Star Patterns

--- Program.cs
+++ Program.cs
@@ -15,9 +15,9 @@ namespace console
             int num;
             Console.WriteLine("Enter Number:");
             num = Convert.ToInt32(Console.ReadLine());
-            for (x = 0; x < num; x++)
+            for (x = 1; x <= num; x++)
             {
-                for (y = 0; y < x; y++)
+                for (y = 1; y <= x; y++)
 
                 {
                     Console.Write("* ");
@@ -27,9 +27,50 @@ namespace console
             Console.ReadLine();
 
 
-        }
-    }
 
+            //Square Pattern Printing Code
+            int a;
+            int b;
+            int side;
+
+            Console.WriteLine("Write side ");
+            side = Convert.ToInt32(Console.ReadLine());
+            for (a = 1; a <= side; a++)
+            {
+                for (b = 1; b <= side; b++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+            //Rectangle Pattern Programming
+
+            int breadth;
+            int length;
+
+            int len;
+            int brth;
+
+            Console.WriteLine("Write Breadth Side");
+            len = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write Length Side");
+            brth = Convert.ToInt32(Console.ReadLine());
+
+            for (breadth = 1; breadth <= brth; breadth++)
+            {
+                for (length = 1; length <= len; length++)
+                {
+                    Console.Write(" + ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
 }
 
 
